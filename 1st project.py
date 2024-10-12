9cddf724b818656a4705f8f1fa1f6225ae4de236
Nikunjprojects <nikunjsaini08@gmail.com>
2024-10-12 21:32:47 +0530

    Add files via upload

---
+++ 1st project.py
@@ -0,0 +1,10 @@
+# Write a program to demonstrate different number data types in Python.
+
+x = 34
+y = 23.0
+z = 45+78j
+k = True
+print("The value of x is",x,type(x))
+print("The value of y is",y,type(y))
+print("The value of z is",z,type(z))
+print("The value of k is",k,type(k))
