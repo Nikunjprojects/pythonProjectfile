ba33108b6d93ad3c587a04e137003c9e855d9407
Nikunjprojects <nikunjsaini08@gmail.com>
2024-10-12 21:54:14 +0530

    Add files via upload

---
+++ 3rd project.py
@@ -0,0 +1,19 @@
+# Write a program to create, concatenate and print a string and accessing sub-string  from a given string.
+
+str = "hello"
+str2 = "nikunj"
+
+str3 = str +"   "+ str2
+print(str3)
+
+# string slicing
+hi = "who are you "
+lenght = len(hi)
+print(lenght)
+print(hi[0:5])
+# string slicing with index only
+print(hi[:-5])
+my_string = "Hello, World!"
+# Extracting every second character
+substring = my_string[::2]
+print(substring)
