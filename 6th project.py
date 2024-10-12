218e8cd0596513cefa1a72032724cd8ff340f69e
Nikunjprojects <nikunjsaini08@gmail.com>
2024-10-12 22:07:15 +0530

    Add files via upload

---
+++ 6th project.py
@@ -0,0 +1,7 @@
+# Write a programme to add two number in python by user input.
+
+
+num1 = float(input("enter your first number here:"))
+num2 = float(input("enter your second number here:"))
+print("the sum is ")
+print(num1 +num2)
