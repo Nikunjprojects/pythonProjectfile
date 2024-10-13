27bad60aca252259d4b77deae507fb98be6c07ba
Nikunjprojects <nikunjsaini08@gmail.com>
2024-10-13 22:04:30 +0530

    Add files via upload

---
+++ 8 project.py
@@ -0,0 +1,12 @@
+# Write a programme to perform assignment operation in python
+
+a = int(input("enter your first number "))
+b = int(input("enter your second number "))
+
+# Printing the assigned values
+print("Value of a:",a)
+print("Value of b:",b)
+
+a += 5
+print("Updated value of a:",a)
+print("Updated value of b:",b)
