8c2fe0fa3e9f44b584060c008dc383b5a1e7d902
Nikunjprojects <nikunjsaini08@gmail.com>
2024-10-13 22:07:50 +0530

    Add files via upload

---
+++ 9th project.py
@@ -0,0 +1,17 @@
+# Write a programme to perform comparison operation in python?
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print(f"Comparing {num1} and {num2}:")
+
+if (num1 == num2):
+    print(f" {num1} equal to {num2}")
+
+elif{num1<num2}:
+    print(f"{num1} lesser than {num2}")
+else:
+    print(f"{num1} greaterrthan {num2}")
+
+
+
+
